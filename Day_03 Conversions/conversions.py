8880c826ed2f9d79a5d497c8b9189540a8601837
PrabhuDeva2001 <puliprabhudeva@gmail.com>
2025-12-02 09:27:44 +0530

    Day-3 Conversions

---
+++ Day_03 Conversions/conversions.py
@@ -0,0 +1,27 @@
+#int conversions
+a=10
+print(float(a))
+print(bool(a))
+print(complex(a))
+print(str(a))
+
+#Float 
+b=21.5
+print(int(b))
+print(bool(b))
+print(str(b))
+print(complex(b))
+
+#complex
+c=12+1j
+
+#print(int(c))
+#print(float(c))
+#print(bool(c))
+#print(str(c))
+
+d=True
+
+print(int(d))
+print(bool(d))
+print(str(d))
